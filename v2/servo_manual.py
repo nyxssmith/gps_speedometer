363f806a6dd928ccee6b6801cfed4fd3e8bb6626
Nick <nickswimz@gmail.com>
2020-05-04 15:24:30 -0400

    debug manul aservo with python2?

--- v2/servo_manual.py
+++ v2/servo_manual.py
@@ -30,6 +30,7 @@ def send_pwm_to_servo(pwm, servo):
     global last_pwm,current_pwm
     # if theres a change to the pwm
     if float(pwm) != float(last_pwm):
+        print("sending pwm to servo old:new",current_pwm,pwm)
         last_pwm = copy.copy(current_pwm)
         current_pwm = pwm
         GPIO.output(servoPIN, True)
